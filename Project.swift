e3f4be10d33f069d8f0b93ba6fad71ac31aa27c3
scgopi <scgopireddy@gmail.com>
2026-08-13 19:32:50 -0700

    0.1.39-beta1 (build 126): version bump for export/import feature

--- Project.swift
+++ Project.swift
@@ -54,8 +54,8 @@ let project = Project(
                 // (#33). Before that the suffix lived on the tag only, so betas 48/49
                 // of the 0.1.15 line read "0.1.15" and are told by the build number
                 // apart.
-                "CFBundleShortVersionString": "0.1.38",
-                "CFBundleVersion": "125",
+                "CFBundleShortVersionString": "0.1.39-beta1",
+                "CFBundleVersion": "126",
             ]),
             resources: [
                 "graphcode/Resources/**"
