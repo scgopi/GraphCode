29de6701660b4f6932784d2e441d13d4454552f7
scgopi <scgopireddy@gmail.com>
2026-08-23 18:25:50 -0700

    0.1.47 (build 173): version bump

--- Project.swift
+++ Project.swift
@@ -54,8 +54,8 @@ let project = Project(
                 // (#33). Before that the suffix lived on the tag only, so betas 48/49
                 // of the 0.1.15 line read "0.1.15" and are told by the build number
                 // apart.
-                "CFBundleShortVersionString": "0.1.47-beta1",
-                "CFBundleVersion": "172",
+                "CFBundleShortVersionString": "0.1.47",
+                "CFBundleVersion": "173",
             ]),
             resources: [
                 "graphcode/Resources/**"
