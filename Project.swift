eb483855fa42d90c60709afc01192db498ee0719
scgopi <scgopireddy@gmail.com>
2026-08-27 21:03:30 -0700

    release: 0.1.53-beta1

--- Project.swift
+++ Project.swift
@@ -54,8 +54,8 @@ let project = Project(
                 // (#33). Before that the suffix lived on the tag only, so betas 48/49
                 // of the 0.1.15 line read "0.1.15" and are told by the build number
                 // apart.
-                "CFBundleShortVersionString": "0.1.52",
-                "CFBundleVersion": "203",
+                "CFBundleShortVersionString": "0.1.53-beta1",
+                "CFBundleVersion": "204",
             ]),
             resources: [
                 "graphcode/Resources/**"
